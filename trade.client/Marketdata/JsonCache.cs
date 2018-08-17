06da9eea3b9f5ad06bf0601676a40765f4a05075
yinwenbo <yinwenbo@hotmail.com>
2018-08-17 10:07:11 +0800

    commit

--- trade.client/Marketdata/JsonCache.cs
+++ trade.client/Marketdata/JsonCache.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace trade.client.Marketdata
@@ -19,16 +20,32 @@ namespace trade.client.Marketdata
             LoadFromFile();
         }
 
+        ~JsonCache()
+        {
+            SaveToFile();
+        }
+
+        public Timer timer;
         public string FileName { get; private set; }
         public Action<List<T>> OnCacheUpdated { set; get; }
         public Func<T, string> BuildCacheKey { get; private set; }
 
         public void Update(List<T> list)
         {
-            list.ForEach((data) => { Caches[BuildCacheKey(data)] = data; });
-            SaveToFile();
+            lock (Caches)
+            {
+                list.ForEach((data) => { Caches[BuildCacheKey(data)] = data; });
+            }
             OnCacheUpdated?.Invoke(list);
+            timer = new Timer(OnTimerCallback, null, 1 * 1000, 30 * 1000);
         }
+
+        private void OnTimerCallback(object state)
+        {
+            //Thread thread = new Thread(SaveToFile);
+            //thread.Start();
+        }
+
         public T Get(string key)
         {
             Caches.TryGetValue(key, out T result);
@@ -37,8 +54,16 @@ namespace trade.client.Marketdata
 
         public void SaveToFile()
         {
-            string json = JsonConvert.SerializeObject(Caches, Formatting.Indented);
-            File.WriteAllText(FileName, json);
+            Dictionary<string, T> data;
+            lock (Caches)
+            {
+                data = new Dictionary<string, T>(Caches);
+            }
+            lock(this)
+            {
+                string json = JsonConvert.SerializeObject(data, Formatting.Indented);
+                File.WriteAllText(FileName, json);
+            }
         }
         public void LoadFromFile()
         {
